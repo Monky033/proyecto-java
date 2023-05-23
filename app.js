8e4210ed882652b585feb5efee9e433addbaa55e
juan ignacio moreno <juanimignacio@gmail.com>
2023-05-23 13:05:48 -0300

    Nuevo Diseño

--- app.js
+++
@@ -1,9 +0,0 @@
-let edad = prompt("¿Cuantos años tenes?"); 
-while (edad === "" || isNaN(edad)) {
-    edad = prompt("ponela con números");
-}
-if (edad >= 18) { 
-    console.log("PASAAA REYY");
-} else { 
-    console.log("NOO sos menor, anda a dormir");
-}
---
+++ js/productos.js
@@ -0,0 +1,67 @@
+const productos = [
+    {
+    id: 1,nombre: "Tiesa",precio: 50,
+    img:
+        "https://pbs.twimg.com/media/Fj_JPugXgAE_T87?format=jpg&name=900x900",
+    cantidad: 1,
+    },
+    {
+    id: 2,nombre: "Adios",precio: 100,
+    img:
+        "https://media.licdn.com/dms/image/C4D22AQGRE8MZCaOhsg/feedshare-shrink_2048_1536/0/1668099745727?e=1687996800&v=beta&t=5Tnlyl97mc5eZMp5MS3zVnweHeWSvYQD3FTJqnnS3HM",
+    cantidad: 1,
+    },
+    {
+    id: 3,nombre: "Pena",precio: 150,
+    img:
+        "https://i.pinimg.com/564x/47/7d/cb/477dcb4e1c0237da6733d22c75c28d0d.jpg",
+    cantidad: 1,
+    },
+    {    id: 4, nombre: "Yendo",precio: 200,
+    img:
+        "https://i.pinimg.com/564x/63/9f/c6/639fc602d239a751069667eb78e0385e.jpg",
+    cantidad: 1,
+    },
+    {    id: 4, nombre: "Dando Todo",precio: 200,
+    img:
+        "https://i.pinimg.com/564x/3a/ed/3a/3aed3a5214838c94054bd334a81e2baa.jpg",
+    cantidad: 1,
+    },
+    { id: 1,nombre: "Calculadisimo",precio: 50,
+    img:
+        "https://i.pinimg.com/564x/0c/5b/01/0c5b01b2cafd5e4104e5fa74e93d4de8.jpg",
+        cantidad: 1,
+    },
+    {
+    id: 2,nombre: "Saluu",precio: 100,
+    img:
+        "https://i.pinimg.com/564x/00/ae/9f/00ae9f45ed01a9862c9c54ba5423bac8.jpg",
+    cantidad: 1,
+    },
+    {
+    id: 3,nombre: "Recalculando",precio: 150,
+    img:
+        "https://i.pinimg.com/564x/24/8b/ae/248bae4da5634bc50732ee9588875f92.jpg",
+    cantidad: 1,
+    },
+    {    id: 4, nombre: "Nos Copiamos",precio: 200,
+    img:
+        "https://i.pinimg.com/564x/f5/2b/ee/f52beed64a0a1bf07335ce62349ecf3f.jpg",
+    cantidad: 1,
+    },
+    {    id: 4, nombre: "MMM",precio: 200,
+    img:
+        "https://i.pinimg.com/564x/8f/e6/d7/8fe6d71e7313e21461ff13fe3436ffbb.jpg",
+    cantidad: 1,
+    },
+    {    id: 4, nombre: "Producida",precio: 200,
+    img:
+        "https://i.pinimg.com/564x/2b/cd/bd/2bcdbd9518f76762dae9371b9b82995e.jpg",
+    cantidad: 1,
+    },
+    {    id: 4, nombre: "QUE",precio: 200,
+    img:
+        "https://i.pinimg.com/564x/77/dd/18/77dd18f96b9adf1ae977379da4dd953c.jpg",
+    cantidad: 1,
+    },
+];
